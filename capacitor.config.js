b293a0cfeb3725a1cf2493324ac2adf40d4245bd
jamesbishopemmanuel-cloud <jamesbishopemmanuel@gmail.com>
2026-08-22 20:51:08 +0100

    Create capacitor.config.js

---
+++ capacitor.config.js
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config = {
+  appId: "com.veylora.app",
+  appName: "Veylora",
+  webDir: "dist",
+  bundledWebRuntime: false
+};
+
+export default config;
