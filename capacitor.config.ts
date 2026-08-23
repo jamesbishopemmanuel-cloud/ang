8c3071530a88f3878ec3e06208e907f6dde63e3b
jamesbishopemmanuel-cloud <jamesbishopemmanuel@gmail.com>
2026-08-23 08:17:34 +0100

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from "@capacitor/cli";
-
-const config: CapacitorConfig = {
-  appId: "com.veylora.app",
-  appName: "Veylora",
-  webDir: "dist"
-};
-
-export default config;
