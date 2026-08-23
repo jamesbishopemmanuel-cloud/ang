dcf138039b02ecd90c1df679a238a957e4af653b
jamesbishopemmanuel-cloud <jamesbishopemmanuel@gmail.com>
2026-08-23 08:20:23 +0100

    Update capacitor.config.js

--- capacitor.config.js
+++ capacitor.config.js
@@ -1,24 +1,8 @@
-
 const config = {
   appId: "com.veylora.app",
   appName: "Veylora",
   webDir: "dist",
-
-  server: {
-    androidScheme: "https"
-  },
-
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      backgroundColor: "#ffffff",
-      showSpinner: true
-    }
-  },
-
-  android: {
-    allowMixedContent: true
-  }
+  bundledWebRuntime: false,
 };
 
-export default config;
+module.exports = config;
